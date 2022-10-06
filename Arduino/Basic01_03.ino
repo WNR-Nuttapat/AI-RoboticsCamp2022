1fbb391728d5f499f9669b3e69046ea1954cf8a3
Nuttapat Tangsatienkij <79248785+WNR-Nuttapat@users.noreply.github.com>
2022-10-07 02:59:29 +0700

    Create Basic01_03.ino

---
+++ Arduino/Basic01_03.ino
@@ -0,0 +1,19 @@
+bool status1 = 0;
+bool status2 = LOW;
+byte val1 = 260 , val2 = 150;
+char ch1 = 'A'; char ch2 = 65;
+int val3 = 900; int val4 = 70000; float val5 = 22.0/7;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.print(status1); Serial.print(' '); Serial.println(status2);
+  Serial.print(val1); Serial.print(' '); Serial.println(val2);
+  Serial.print(ch1); Serial.print(' '); Serial.println(ch2);
+  Serial.print(val3); Serial.print(' '); Serial.println(val4);
+  Serial.println(val5);  
+  delay(1000);
+}
