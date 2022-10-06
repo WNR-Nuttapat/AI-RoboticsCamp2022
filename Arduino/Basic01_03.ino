240329ab284cf36d376d492fc582af74de9e473d
Nuttapat Tangsatienkij <79248785+WNR-Nuttapat@users.noreply.github.com>
2022-10-07 03:16:00 +0700

    Update Basic01_03.ino

--- Arduino/Basic01_03.ino
+++ Arduino/Basic01_03.ino
@@ -1,8 +1,8 @@
 bool status1 = 0;
 bool status2 = LOW;
-byte val1 = 260 , val2 = 150;
+byte val1 = 150 , val2 = 260;
 char ch1 = 'A'; char ch2 = 65;
-int val3 = 900; int val4 = 70000; float val5 = 22.0/7;
+int val3 = 70000; int val4 = 900; float val5 = 22.0/7;
 void setup() {
   // put your setup code here, to run once:
   Serial.begin(9600);
@@ -14,6 +14,8 @@ void loop() {
   Serial.print(val1); Serial.print(' '); Serial.println(val2);
   Serial.print(ch1); Serial.print(' '); Serial.println(ch2);
   Serial.print(val3); Serial.print(' '); Serial.println(val4);
-  Serial.println(val5);  
+  Serial.println(val5);
+  int val3=500;
+  Serial.println(val3);   
   delay(1000);
 }
