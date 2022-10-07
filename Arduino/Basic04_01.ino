0afe66e55ca714b85bb0a97478df7336a5e243ed
Nuttapat Tangsatienkij <79248785+WNR-Nuttapat@users.noreply.github.com>
2022-10-07 17:50:44 +0700

    Create Basic04_01.ino

---
+++ Arduino/Basic04_01.ino
@@ -0,0 +1,12 @@
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  int pot = analogRead(A0);
+  Serial.println(pot);
+  analogWrite(10,255);
+  delay(1000);
+}
